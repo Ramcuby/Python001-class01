76414cd93ce884f0ca27d15c1a75178e8ffc7459
Ramcuby <onlyu52@163.com>
2020-07-18 11:19:11 +0800

    revise to a1

--- week03/pmap.py
+++ week03/pmap.py
@@ -2,20 +2,42 @@
 import argparse
 import json
 import os
+import ipaddress
+import functools
 import sys
+import multiprocessing
 import subprocess
+import shlex
 import socket
 import threading
 import time
+from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor
+
+print_lock = multiprocessing.Lock()
 
 net='192.168.10'
-def ping(ip,):
-    ret = subprocess.call("ping -c 2 -W 1 %s" % ip,shell=True,stdout=subprocess.PIPE)
-    if ret == 0:
-        aliveIPList.append(ip)
-        return(1)
-    elif ret == 1:
-        return(0)
+def validate_ip(ip):
+    return ipaddress.IPv4Address(ip)
+
+def get_ip_list_in_range(start,end):
+    for ips in ipaddress.summarize_address_range(ipaddress.IPv4Address(start),ipaddress.IPv4Address(end)):
+        yield from ips
+
+def check_host_with_ping(ip):
+    result = subprocess.run(
+        shlex.split("ping {ip} -c 3 -t 3".format(ip=str(ip))),
+        stdout=subprocess.DEVNULL,
+        stderr=subprocess.DEVNULL
+    )
+    if result.returncode == 0:
+        with print_lock:
+            print(ip)
+    # ret = subprocess.call("ping -c 2 -W 1 %s" % ip,shell=True,stdout=subprocess.PIPE)
+    # if ret == 0:
+    #     aliveIPList.append(ip)
+    #     return(1)
+    # elif ret == 1:
+    #     return(0)
             
 def creatThread(net):
     myThread = []
@@ -32,15 +54,18 @@ def creatThread(net):
     for i in range(len(myThread)):
         myThread[i].join()
         
-def scan(host, port):
-    try:
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def check_port_with_tcp(host, port):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.settimeout(3)
+    if s.connect_ex((str(host),port))==0:
+        with print_lock:
+            print(port)    
         # 进行连接
-        s.connect((host, port))
+        # s.connect((host, port))
         # 连接成功就添加到开放端口列表
-        openPortList.append(port)
-    except:
-        pass
+        # openPortList.append(port)
+    # except:
+        # pass
     
 def scanThread(host, portMax):
     myThread = []
@@ -56,66 +81,71 @@ def scanThread(host, portMax):
     for i in range(len(myThread)):
         myThread[i].join()
         
-
+def main(args):
+    if "ping" == args["f"]:
+        ip_list = get_ip_list_in_range(*(validate_ip(ip) for ip in args["ip"].split("-")))
+        with ProcessPoolExecutor(args["n"]) as pool:
+            pool.map(check_host_with_ping,ip_list)
+    elif "tcp" == args["f"]:
+        with ThreadPoolExecutor(args["n"]) as pool:
+            pool.map(functools.partial(check_port_with_tcp,validate_ip(args["ip"])),range(1,1025))
+        
         
 if __name__ == '__main__':
-    hostname=socket.gethostname()
-    localHost =socket.gethostbyname(hostname)
+    # hostname=socket.gethostname()
+    # localHost =socket.gethostbyname(hostname)
 
-    parser = argparse.ArgumentParser(description=" argparse of pmap")
+    parser = argparse.ArgumentParser(prog="pmap",description=" argparse of pmap")
 
-    parser.add_argument('-n','--numconcur',help="number of concurrency", default='4',type=int)
-    parser.add_argument('-f','--conntype',help="connection type", default='ping',type=str,choices=['ping','tcp'])
-    parser.add_argument('-ip','--ip',help="set ip or ip range", default='localHost')
-    parser.add_argument('-w','--filename',help="write to a file name", default='result.json')
- 
+    parser.add_argument("-n",help="number of concurrency", type=int,nargs="?",required=True)
+    parser.add_argument("-f",nargs="?",help="connection type", default="ping",choices=["ping","tcp"],required=True)
+    parser.add_argument("-ip",help="set ip or ip range", default="localHost",required=True)
+    parser.add_argument("-w",help="write to a file name", default="result.json")
     args = parser.parse_args()
-
-    numconcur = args.numconcur
-    conntype = args.conntype
-    ip = args.ip
-    filename = args.filename
-
-#     while True:
-    aliveIPList=[]
-    try:
-        creatThread(net)
-        aliveIPList.sort()
-#         print(aliveIPList)
-        for aliveIP in aliveIPList:
-            print("aliveIP:",aliveIP)
-    except:
-        print("Input Error！")
+    # print(args)
     
-    openPortList = []
-    inputList = list((aliveIPList[0]+' '+'1000').split())
-    print(inputList)
-    start = time.time() # 计时开始
+    main(vars(args))
 
-    print("Scaning……")
-
-    scanThread(*inputList)
-    print(*inputList)
-    print("---------------------------")
-    name_emb = {}
-    for openPort in openPortList:
-        print(openPort, ": Open")
-        key1=int(openPort)
-    name_emb=dict.fromkeys(openPortList,'open')
-    # print(name_emb)
-        
-    output_dir = './'   
-    emb_filename = os.path.join(output_dir, filename)
- 
-    jsObj = json.dumps(name_emb)  
- 
-    with open(emb_filename, "w") as f:
-        f.write(jsObj)
-        f.close()
-
+# #     while True:
+#     aliveIPList=[]
+#     try:
+#         creatThread(net)
+#         aliveIPList.sort()
+# #         print(aliveIPList)
+#         for aliveIP in aliveIPList:
+#             print("aliveIP:",aliveIP)
 #     except:
-#             print("Input Error！-2")
-    end = time.time() # 计时结束
-    print("time-consuming：%.2fs" %(end - start))
-    print("---------------------------")
+#         print("Input Error！")
+    
+#     openPortList = []
+#     inputList = list((aliveIPList[0]+' '+'1000').split())
+#     print(inputList)
+#     start = time.time() # 计时开始
+
+#     print("Scaning……")
+
+#     scanThread(*inputList)
+#     print(*inputList)
+#     print("---------------------------")
+#     name_emb = {}
+#     for openPort in openPortList:
+#         print(openPort, ": Open")
+#         key1=int(openPort)
+#     name_emb=dict.fromkeys(openPortList,'open')
+#     # print(name_emb)
+        
+#     output_dir = './'   
+#     emb_filename = os.path.join(output_dir, filename)
+ 
+#     jsObj = json.dumps(name_emb)  
+ 
+#     with open(emb_filename, "w") as f:
+#         f.write(jsObj)
+#         f.close()
+
+# #     except:
+# #             print("Input Error！-2")
+#     end = time.time() # 计时结束
+#     print("time-consuming：%.2fs" %(end - start))
+#     print("---------------------------")
 
